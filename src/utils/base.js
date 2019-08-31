4ca6892543199fb3fbcc94ca1250aa4e650fd47b
zsh <shaohua.zhou@joojtech.com>
2019-08-31 18:37:07 +0800

    feat:新增assign，支持子属性是对象的递归合并

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,127 +1,155 @@
 /**
  * 深度克隆
  * Object deepclone
- * @param {Object} obj -the object need clone
- * @returns {Object} -clone Object
+ * @param {Object} obj the object need clone
  */
-function deepcloneFunc (obj) {
-    if (typeof obj !== 'object' || obj === null) {
-        return obj;
+function deepclone(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((v) => {
+      if (typeof v === 'object' && v !== null) {
+        return deepclone(v);
+      } else {
+        return v;
+      }
+    });
+  } else {
+    const newObj = {};
+
+    Object.keys(obj).forEach((v) => {
+      if (typeof obj[v] === 'object' && obj[v] !== null) {
+        newObj[v] = deepclone(obj[v]);
+      } else {
+        newObj[v] = obj[v];
+      }
+    });
+
+    return newObj;
+  }
+}
+
+/**
+ * @param {Object} target -目标对象
+ * @param {...Object} source -原对象 
+ */
+function assign(target, ...source) {
+  const newObj = target;
+  if (source.length > 1) {
+    let newTarget = source[0];
+    source.splice(0, 1);
+    source = assign(newTarget, ...source);
+  } else {
+    source = source[0];
+  }
+  Object.keys(newObj).forEach((v) => {
+    if (typeof newObj[v] === 'object' && newObj[v] !== null && typeof source[v] === 'object' && source[v] !== null) {
+      newObj[v] = assign(newObj[v], source[v]);
+      delete source[v];
     }
-
-    if (Array.isArray(obj)) {
-        return obj.map((v) => {
-            if (typeof v === 'object' && v !== null) {return deepclone(v);} else {return v;}
-        });
-    } else {
-        const newObj = {};
-
-        Object.keys(obj).forEach((v) => {
-            if (typeof obj[v] === 'object' && obj[v] !== null) {
-                newObj[v] = deepclone(obj[v]);
-            } else {
-                newObj[v] = obj[v];
-            }
-        });
-
-        return newObj;
-    }
+  });
+  Object.keys(source).forEach((v) => {
+    newObj[v] = source[v];
+  });
+  return newObj;
 }
 
 /**
  * 返回空
  * @returns {Object} -null
  */
-function noopFunc () {
-    return null;
+function noop() {
+  return null;
 }
 
 /**
  * 判断属性是否是对象的私有属性 非原型链属性
  * @param {Object} obj -对象
  * @param {string} type -属性
- * @returns {Boolean} 是否是对象的私有属性
+ * @returns {Boolean}
  */
-function hasOwnFunc (obj, type) {
-    return Object.prototype.hasOwnProperty.call(obj, type);
+function hasOwn(obj, type) {
+  return Object.prototype.hasOwnProperty.call(obj, type);
 }
 
 /**
  * 判断变量是否未定义
- * @param {any} item -需要判断的
- * @returns {Boolean} -是否未定义
+ * @param  item
+ * @returns {Boolean}
  */
-function isUndefFunc (item) {
-    return item === undefined || item === null;
+function isUndef(item) {
+  return item === undefined || item === null;
 }
 
 /**
  * 判断变量是否定义
- * @param {any} item -需要判断的变量
- * @returns {Boolean} -是否定义
+ * @param item
+ * @returns {Boolean}
  */
-function isDefFunc (item) {
-    return !this.isUndefined(item);
+function isDef(item) {
+  return !this.isUndefined(item);
 }
 
 /**
  * 判断是否是字符串
- * @param {any} item -需要判断的变量
- * @returns {Boolean} -是否是字符串
+ * @param {any} item
+ * @returns {Boolean}
  */
-function isStrFunc (item) {
-    return typeof item === 'string';
+function isStr(item) {
+  return typeof item === 'string';
 }
 
 /**
  * 判断是否是数字
- * @param {any} item -需要判断的变量
- * @returns {Boolean} -是否是数字
+ * @param {any} item
+ * @returns {Boolean}
  */
-function isNumberFunc (item) {
-    return typeof item === 'number';
+function isNumber(item) {
+  return typeof item === 'number';
 }
 
 /**
  * 判断是否是数组
- * @param {any} item -需要判断的变量
- * @returns {Boolean} -是否是数组
+ * @param {any} item
+ * @returns {Boolean}
  */
-function isArrayFunc (item) {
-    // 判断是否有 isArray方法 如果有绑定当前content 返回
-    if (typeof Array.isArray === 'function') {
-        return Array.isArray.bind(this)(item);
-    }
-    // pollfill
-    return Object.prototype.toString.call(item) === '[object Array]';
+function isArray(item) {
+  // 判断是否有 isArray方法 如果有绑定当前content 返回
+  if (typeof Array.isArray === 'function') {
+    return Array.isArray.bind(this)(item);
+  }
+  // pollfill
+  return Object.prototype.toString.call(item) === '[object Array]';
 }
 
 /**
  * 判断是否是对象
- * @param {any} obj  -需要判断的变量
- * @returns {Boolean} -是否是对象
+ * @param {any} item
+ * @returns {Boolean}
  */
-function isObjFunc (obj) {
-    return obj !== null && typeof obj === 'object';
+function isObj(obj) {
+  return obj !== null && typeof obj === 'object';
 }
 
 /**
  * 判断是否是函数
- * @param {any} item -需要判断的变量
- * @returns {Boolean} -是否是对象
+ * @param {any} item
+ * @returns {Boolean}
  */
-function isFuncFunc (item) {
-    return typeof item === 'function';
+function isFunc(item) {
+  return typeof item === 'function';
 }
 
 /**
  * 转数字
- * @param {any} val -需要判断的变量
- * @returns {any} -返回的值
+ * @param {any} val
+ * @returns {(typeof val | number)} -返回的值
  */
-function toNumberFunc (val) {
-    let n = parseFloat(val);
-    return isNaN(n) ? val : n;
+function toNumber(val) {
+  let n = parseFloat(val);
+  return isNaN(n) ? val : n;
 }
 
 
@@ -131,15 +159,41 @@ function toNumberFunc (val) {
  * @see module:utils/base
  */
 
-export const deepclone = deepcloneFunc;
-export const noop = noopFunc;
-export const hasOwn = hasOwnFunc;
-export const isUndef = isUndefFunc;
-export const isDef = isDefFunc;
-export const isStr = isStrFunc;
-export const isNumber = isNumberFunc;
-export const isArray = isArrayFunc;
-export const isObj = isObjFunc;
-export const isFunc = isFuncFunc;
-export const toNumber = toNumberFunc;
+module.exports = {
 
+  /** 深拷贝 */
+  deepclone: deepclone,
+
+  /** 返回空 */
+  noop: noop,
+
+  /** 是否私有属性 */
+  hasOwn: hasOwn,
+
+  /** 是否未定义 */
+  isUndef: isUndef,
+
+  /** 是否定义 */
+  isDef: isDef,
+
+  /** 是否是字符串 */
+  isStr: isStr,
+
+  /** 是否是数字 */
+  isNumber: isNumber,
+
+  /** 是否是数组 */
+  isArray: isArray,
+
+  /** 是否是对象 */
+  isObj: isObj,
+
+  /** 是否是函数 */
+  isFunc: isFunc,
+
+  /** 尝试转换数字 */
+  toNumber: toNumber,
+
+  /** 递归assign */
+  assign:assign,
+};
